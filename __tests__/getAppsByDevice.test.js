480d30eaa774f83cd3791a57f395fa490bd5e4f2
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2019-04-09 21:22:55 -0400

    Tests should be runnable from anywhere

--- __tests__/getAppsByDevice.test.js
+++ __tests__/getAppsByDevice.test.js
@@ -1,5 +1,25 @@
 const getAppsByDevice = require('../getAppsByDevice');
 
 it('should get devices correctly', () => {
-  expect(getAppsByDevice(`${__dirname}/Devices`)).toMatchSnapshot();
+  const appsByDevice = getAppsByDevice(`${__dirname}/Devices`);
+
+  // We have to trim the absolute path as `homedir()` will be different for everyone.
+  const trimPaths = appsByDevice.map(device => {
+    if (device.apps.length > 0) {
+      return {
+        ...device,
+        path: device.path.split('__tests__')[1],
+        apps: device.apps.map(app => ({
+          ...app,
+          file: app.file.split('__tests__')[1],
+        })),
+      };
+    }
+
+    return {
+      ...device,
+      path: device.path.split('__tests__')[1],
+    };
+  });
+  expect(trimPaths).toMatchSnapshot();
 });
